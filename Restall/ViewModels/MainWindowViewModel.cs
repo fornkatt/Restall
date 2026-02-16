cb17b5167b80fd5d83cfa9a5d69747556be53d54
Johan Lager <johan.l@tuta.com>
2026-02-16 14:11:22 +0100

    - Initial commit

---
+++ Restall/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Restall.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Restall/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Restall.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
