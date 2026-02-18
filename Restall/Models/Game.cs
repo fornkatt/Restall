a9d2db898e56d68c786815ee5fc8947f07bb95d4
Johan Lager <johan.l@tuta.com>
2026-02-18 16:10:47 +0100

    Removed GameLibrary class. Added properties to Game, RenoDX and ReShade models with ObservableObject methods to check for property changed events.

--- Restall/Models/Game.cs
+++ Restall/Models/Game.cs
@@ -1,26 +1,62 @@
-
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+// ReSharper disable InconsistentNaming
 
 namespace Restall.Models;
 
-public class Game
+public class Game : ObservableObject
 {
-    
-    public required string Name { get; set; }
-    public string? PlatformName { get; set; }
-    
+    public string? Name { get; init; }
+    public string? PlatformName { get; init; }
+
+    public string? ExecutableName { get; set; }
     public string? ExecutablePath { get; set; }
     public string? InstallFolder { get; set; }
+
+    private string? _thumbnailPath;
+    private string? _bannerPath;
+    private string? _logoPath;
+
+    public string? RenoDXVersion => HasRenoDX ? "2026-02-18" : "Not installed";
+    public string? ReShadeVersion => HasReShade ? "6.7.2" : "Not installed";
+
+    private bool _hasReShade;
+    private bool _hasRenoDX;
+    private bool _isInstalled;
+
+    public string? LogoPath
+    {
+        get => _logoPath;
+        set => SetProperty(ref _logoPath, value);
+    }
     
-    public string? ThumbnailPath { get; set; }
-    public string? BannerPath {get; set; }
+    public string? ThumbnailPath
+    {
+        get => _thumbnailPath;
+        set => SetProperty(ref _thumbnailPath, value);
+    }
 
-    public RenoDX? RenoDX { get; set; }
-    public ReShade? ReShade { get; set; }
-
-    public bool HasReShade { get; set; } = false;
-    public bool HasRenoDX { get; set; } = false;
-    public bool IsInstalled { get; set; } = false;
-
-
+    public string? BannerPath
+    {
+        get => _bannerPath;
+        set => SetProperty(ref _bannerPath, value);
+    }
+    
+    public bool HasReShade
+    {
+        get => _hasReShade;
+        set => SetProperty(ref _hasReShade, value);
+    }
+    
+    public bool HasRenoDX
+    {
+        get => _hasRenoDX;
+        set => SetProperty(ref _hasRenoDX, value);
+    }
+    
+    public bool IsInstalled
+    {
+        get => _isInstalled;
+        set => SetProperty(ref _isInstalled, value);
+    }
 }
--- Restall/Models/ReShade.cs
+++ Restall/Models/ReShade.cs
@@ -1,6 +1,17 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace Restall.Models;
 
-public class ReShade
+public class ReShade : ObservableObject
 {
+    public string? Name { get; set; }
+    public string? Version { get; set; }
+    public string? AvailableVersion { get; set; }
     
+    private bool _isInstalled;
+    public bool IsInstalled
+    {
+        get => _isInstalled;
+        set => SetProperty(ref _isInstalled, value);
+    }
 }
