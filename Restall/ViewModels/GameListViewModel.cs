a8ab10427d99b4f3471ed2b560f5a9bc53ff2877
Kristofer Sell <kristoferelissell@gmail.com>
2026-02-19 15:28:14 +0100

    Added GameListViewModel functionality

--- Restall/ViewModels/GameListViewModel.cs
+++ Restall/ViewModels/GameListViewModel.cs
@@ -1,6 +1,42 @@
+using System.Collections.ObjectModel;
+using Restall.Models;
+
 namespace Restall.ViewModels;
 
 public class GameListViewModel : ViewModelBase
 {
-    
+    private Game? _selectedGame;
+
+    //TODO: INSTALLED GAMES AND NOT INSTALLED GAMES OBSERVABLE COLLECTION?
+    public ObservableCollection<Game> Games { get; } = [];
+
+    public Game? SelectedGame
+    {
+        get => _selectedGame;
+        set => SetProperty(ref _selectedGame, value);
+    }
+
+    public GameListViewModel()
+    {
+        Games.Add(new Game { Name = "asdf" });
+        Games.Add(new Game { Name = "qwerty" });
+        Games.Add(new Game { Name = "1234" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+        Games.Add(new Game { Name = "5678" });
+    }
 }
