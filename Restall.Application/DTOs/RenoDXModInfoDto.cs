d8ffb8f2e8210b8cc6ca776b98de64ca423ef859
Johan Lager <johan.l@tuta.com>
2026-03-07 21:34:19 +0100

    Added ability to override filenames for RenoDX addons in RenoDXModInfoDto, for use on addons that are only on Discord and Nexus, we need to add these manually for now. Added ability to extract addon filename from the wiki if a snapshot url is present.

--- Restall.Application/DTOs/RenoDXModInfoDto.cs
+++ Restall.Application/DTOs/RenoDXModInfoDto.cs
@@ -13,7 +13,28 @@ public record RenoDXModInfoDto(
     string? NexusUrl,
     string? Maintainer,
     string? Notes,
-    string? Status
-);
+    string? Status,
+    string? OverrideAddonFileName = null
+)
+
+{
+    public string? AddonFileName64 => ExtractFileName(SnapshotUrl64);
+
+    public string? AddonFileName32 => ExtractFileName(SnapshotUrl32);
+
+    public string? AddonFileName => AddonFileName64 ?? AddonFileName32 ?? OverrideAddonFileName;
+
+    public bool CanAutoInstall => AddonFileName is not null;
+
+    public RenoDXModSource PreferredManualSource =>
+        NexusUrl is not null    ? RenoDXModSource.Nexus :
+        DiscordUrl is not null  ? RenoDXModSource.Discord :
+                                  RenoDXModSource.Unknown;
+
+    private static string? ExtractFileName(string? url) =>
+        Uri.TryCreate(url, UriKind.Absolute, out var uri)
+        ? Path.GetFileName(uri.AbsolutePath)
+        : null;
+}
 
 public enum RenoDXModSource { Snapshot, Nexus, Discord, Unknown }
