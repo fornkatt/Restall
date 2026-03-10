d34aab8666f7d83017a5ede1504b93eb411708b3
Johan Lager <johan.l@tuta.com>
2026-03-10 11:51:09 +0100

    - Fixed not implemented Interface error.

---
+++ Restall.Application/Services/ModManagementFacade.cs
@@ -0,0 +1,47 @@
+﻿using Restall.Application.DTOs;
+using Restall.Application.Interfaces;
+using Restall.Application.UseCases;
+using Restall.Domain.Entities;
+
+namespace Restall.Application.Services;
+
+public class ModManagementFacade : IModManagementFacade
+{
+    private readonly IInstallReShadeUseCase _installReShadeUseCase;
+    private readonly IUninstallReShadeUseCase _uninstallReShadeUseCase;
+    private readonly IInstallRenoDXUseCase _installRenoDXUseCase;
+    private readonly IUninstallRenoDXUseCase _uninstallRenoDXUseCase;
+
+    public ModManagementFacade(
+        IInstallReShadeUseCase installReShadeUseCase,
+        IUninstallReShadeUseCase uninstallReShadeUseCase,
+        IInstallRenoDXUseCase installRenoDXUseCase,
+        IUninstallRenoDXUseCase uninstallRenoDXUseCase
+        )
+    {
+        _installReShadeUseCase = installReShadeUseCase;
+        _uninstallReShadeUseCase= uninstallReShadeUseCase;
+        _installRenoDXUseCase= installRenoDXUseCase;
+        _uninstallRenoDXUseCase = uninstallRenoDXUseCase;
+    }
+
+    public Task<ModOperationResultDto> InstallOrUpdateRenoDXAsync(InstallRenoDXRequest request, IProgress<DownloadProgressReportDto>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ModOperationResultDto> InstallOrUpdateReShadeAsync(InstallReShadeRequest request, IProgress<DownloadProgressReportDto>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ModOperationResultDto> UninstallRenoDXAsync(Game game)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ModOperationResultDto> UninstallReShadeAsync(Game game)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Restall.Infrastructure/Persistance/UserPreferencesRepository.cs
+++ Restall.Infrastructure/Persistance/UserPreferencesRepository.cs
@@ -4,4 +4,13 @@ namespace Restall.Infrastructure.Persistance;
 
 public class UserPreferencesRepository : IUserPreferencesRepository
 {
+    public void LoadAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
